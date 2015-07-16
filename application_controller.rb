7df7001df26fbd5c4a591d2d03e204e78603f1ef
Babster631 <balthazar.olivier@gmail.com>
2015-07-16 14:05:52 +0000

    Updated

--- application_controller.rb
+++ application_controller.rb
@@ -6,5 +6,14 @@ class ApplicationController < Sinatra::Base
   get '/' do
     erb :index
   end
+  
+  get '/map' do
+    erb :map
+  end
+  
+  get '/info' do
+    @disaster = "Peru Earthquake"
+    erb :info
+  end
 
 end
