6610c139890258b763a638f52434ce93ebefcb96
Babster631 <balthazar.olivier@gmail.com>
2015-07-16 15:13:36 +0000

    Updated

--- application_controller.rb
+++ application_controller.rb
@@ -10,9 +10,9 @@ class ApplicationController < Sinatra::Base
   post '/map' do
     @disasterType = params[:disasterType]
     if params[:earthquake]==true
-      @showEarthquakePin = true
+      @showEarthquakePin=true
     elsif params[:tsunami]==true
-      @showTsunamiPin = true
+      @showTsunamiPin=true
     end
     erb :map
   end
